e2bda65cad64d9dd1367130a1882c6112f082fd5
Anatoly Moistrenko <moistrenko@mail.ru>
2023-09-26 16:37:47 +0300

    refactor: trim-symbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,21 +4,25 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
+
 export const trimSymbols = (string, size) => {
   if (size === undefined) return string;
 
   let phrase = "";
   let count = 1;
 
-  string.split("").forEach((letter) => {
+  for (let i = 0; i < string.length; i++) {
+    const letter = string[i];
+
     if (letter !== phrase.at(-1)) {
       count = 1;
     }
+
     if (count <= size) {
       phrase += letter;
       count++;
     }
-  });
+  }
 
   return phrase;
 };
