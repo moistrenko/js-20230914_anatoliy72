5cef13704a66025b6df9d8bed4c72cdafa8bc75b
Anatoly Moistrenko <moistrenko@mail.ru>
2023-09-26 15:48:03 +0300

    feat: invert-object function implemented

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -3,6 +3,14 @@
  * @param {object} obj - the initial object
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
-export function invertObj(obj) {
+export const invertObj = (obj) => {
+  if (obj === undefined) return;
 
-}
+  const newObj = {};
+
+  for (const key in obj) {
+    newObj[obj[key]] = key;
+  }
+
+  return newObj;
+};
