048f12ff54c46c7ab3078db7c71e9158d2b4b911
Anatoly Moistrenko <moistrenko@mail.ru>
2023-09-26 16:14:04 +0300

    feat: trim-symbols function implemented

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,21 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
+export const trimSymbols = (string, size) => {
+  if (size === undefined) return string;
 
-}
+  let phrase = "";
+  let count = 1;
+
+  string.split("").forEach((letter) => {
+    if (letter !== phrase.at(-1)) {
+      count = 1;
+    }
+    if (count <= size) {
+      phrase += letter;
+      count++;
+    }
+  });
+
+  return phrase;
+};
